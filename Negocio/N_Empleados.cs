e300b67d72fd78034b4a17f264a46da3abf31235
Danielpba02 <pereyradaniel2020@gmail.com>
2022-05-07 08:58:37 -0400

    Entidad-Relacion-Empleados1

--- Negocio/N_Empleados.cs
+++ Negocio/N_Empleados.cs
@@ -3,10 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Entidades;
+using Datos;
 
 namespace Negocio
 {
-    internal class N_Empleados
+    public class N_Empleados
     {
+        D_Empleado objDato = new D_Empleado();
+
+        public List<E_Empleados> ListaEmpleados(string buscar)
+        {
+            return objDato.ListaEmpleados(buscar);
+        }
+
+        public void InsertarEmpleados(E_Empleados Empleado)
+        {
+            objDato.InsertarEmpleados(Empleado);
+        }
+
+        public void EditarEmpleados(E_Empleados Empleado)
+        {
+            objDato.EditarEmpleados(Empleado);
+        }
+
+        public void EliminarEmpleados(E_Empleados Empleado)
+        {
+            objDato.EliminarEmpleados(Empleado);
+        }
     }
 }
