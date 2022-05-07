641e4d7a3e1fbfbb056d1e02193c1ffc638ccf82
Danielpba02 <pereyradaniel2020@gmail.com>
2022-05-07 08:18:42 -0400

    Entidad-Relacion-Rol

---
+++ Negocio/N_Rol.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using Datos;
+using Entidades;
+
+
+namespace Negocio
+{
+    internal class N_Rol
+    {
+        D_Rol objMarca = new D_Rol();
+
+        public List<E_Rol> ListaRol(string buscar)
+        {
+            return objMarca.ListaRol(buscar);
+        }
+
+        public void InsertarRol(E_Rol Rol)
+        {
+            objMarca.InsertarRol(Rol);
+        }
+
+        public void EditarRol(E_Rol Rol)
+        {
+            objMarca.EditarRol(Rol);
+        }
+
+        public void EliminarRol(E_Rol Rol)
+        {
+            objMarca.EliminarRol(Rol);
+        }
+    }
+}
